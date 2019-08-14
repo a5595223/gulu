e7225fb126363a4fcf3b5b2e1b1ec68bbef2aa8b
frankfeng <17717444584@163.com>
2019-08-14 20:21:05 +0800

    添加input 及其样式

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ import Icon from './icon.vue'
 import ButtonGroup from './g-button-group'
 import chai from 'chai'
 import spies from 'chai-spies'
+import Input from './input'
 
 const expect = chai.expect
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-input', Input)
 chai.use(spies)
 
 new Vue({
--- src/g-button-group.vue
+++ src/g-button-group.vue
@@ -5,6 +5,7 @@
 </template>
 <script>
 export default {
+  name: "GuluButtonGroup",
   mounted() {
     let name = this.$el.children;
     for (let node of name) {
