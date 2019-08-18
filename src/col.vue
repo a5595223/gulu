9fe7b693185fb106afbc09bad4b9bbe274541fb3
frankfeng <17717444584@163.com>
2019-08-19 00:34:57 +0800

    初步实现响应式

--- src/col.vue
+++ src/col.vue
@@ -4,6 +4,16 @@
   </div>
 </template>
 <script>
+let validator = value => {
+  let keys = Object.keys(value);
+  let valid = true;
+  keys.forEach(key => {
+    if (!["span", "offset"].includes(key)) {
+      valid = false;
+    }
+  });
+  return valid;
+};
 export default {
   name: "GuluCol",
   props: {
@@ -12,6 +22,26 @@ export default {
     },
     offset: {
       type: [Number, String]
+    },
+    phone: {
+      type: Object,
+      validator
+    },
+    ipad: {
+      type: Object,
+      validator
+    },
+    narrowPc: {
+      type: Object,
+      validator
+    },
+    pc: {
+      type: Object,
+      validator
+    },
+    widePc: {
+      type: Object,
+      validator
     }
     // gutter: {
     //   type: [Number, String]
@@ -30,8 +60,37 @@ export default {
       };
     },
     classes() {
-      let { span, offset } = this;
-      return [span && `col-${span}`, offset && `offset-${offset}`];
+      let { span, offset, phone, ipad, narrowPc, pc, widePc } = this;
+      let phoneClass = [];
+      if (phone) {
+        phoneClass = [`col-phone-${phone.span}`];
+      }
+      if (ipad) {
+        phoneClass = [`col-ipad-${ipad.span}`];
+      }
+      if (narrowPc) {
+        phoneClass = [`col-narrow-pc-${narrowPc.span}`];
+      }
+      if (pc) {
+        phoneClass = [`col-pc-${pc.span}`];
+      }
+      if (widePc) {
+        phoneClass = [`col-wide-pc-${widePc.span}`];
+      }
+      return [
+        span && `col-${span}`,
+        offset && `offset-${offset}`,
+        phone && `col-phone-${phone.span}`,
+        ipad && `col-ipad-${ipad.span}`,
+        narrowPc && `col-narrow-pc-${narrowPc.span}`,
+        pc && `col-pc-${pc.span}`,
+        widePc && `col-wide-pc-${widePc.span}`
+        // ...(phone && [`col-phone-${phone.span}`])
+        // ...(ipad && [`col-ipad-${ipad.span}`]),
+        // ...(narrowPc && [`col-narrow-pc-${narrowPc.span}`]),
+        // ...(pc && [`col-pc-${pc.span}`]),
+        // ...(widePc && [`col-wide-pc-${widePc.span}`])
+      ];
     }
   }
 };
@@ -52,6 +111,76 @@ export default {
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       margin-left: ($n/24) * 100%;
+    }
+  }
+  @media (max-width: 576px) {
+    $class-prefix: col-phone-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n/24) * 100%;
+      }
+    }
+    $class-prefix: offset-phone-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n/24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 577px) and (max-width: 768px) {
+    $class-prefix: col-ipad-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n/24) * 100%;
+      }
+    }
+    $class-prefix: offset-ipad-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n/24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 769px) and (max-width: 992px) {
+    $class-prefix: col-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n/24) * 100%;
+      }
+    }
+    $class-prefix: offset-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n/24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 993px) and (max-width: 1200px) {
+    $class-prefix: col-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n/24) * 100%;
+      }
+    }
+    $class-prefix: offset-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n/24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 1201) {
+    $class-prefix: col-wide-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n/24) * 100%;
+      }
+    }
+    $class-prefix: offset-wide-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n/24) * 100%;
+      }
     }
   }
 }
