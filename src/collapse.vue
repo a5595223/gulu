cf3820e3c585030e016a44cc08e39e38ee472382
frankfeng <17717444584@163.com>
2019-11-11 21:44:56 +0800

    添加 默认selected项

--- src/collapse.vue
+++ src/collapse.vue
@@ -11,6 +11,9 @@ export default {
     single: {
       type: Boolean,
       default: false
+    },
+    selected: {
+      type: String
     }
   },
   data() {
@@ -22,6 +25,9 @@ export default {
         eventBus: this.eventBus
       };
     }
+  },
+  mounted() {
+    this.eventBus.$emit("update:selected", this.selected);
   }
 };
 </script>
